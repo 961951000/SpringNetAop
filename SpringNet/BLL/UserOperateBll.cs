f771c16ba8e40c45f22ebbb7066fb2a161a7bb46
祥伟 蔡 <c961951000@outlook.com>
2017-05-18 05:28:11 +0800

    update

--- SpringNet/BLL/UserOperateBll.cs
+++ SpringNet/BLL/UserOperateBll.cs
@@ -1,4 +1,7 @@
-﻿using SpringNet.DAL;
+﻿using Spring.Aop.Framework;
+using SpringNet.DAL;
+using SpringNet.DAL.Interface;
+using SpringNetAop.Aspects;
 using SpringNetAop.Attributes;
 using SpringNetAop.SpringNetHelp;
 
@@ -9,6 +12,13 @@ namespace SpringNet.BLL
         [LoggingAttributes]
         public override string UserLogin(string username)
         {
+            /*ProxyFactory factory = new ProxyFactory(new UserOperateDal());
+            factory.AddAdvice(new LoggingAroundAdvice());
+            factory.AddAdvice(new LoggingBeforeAdvice());
+            factory.AddAdvice(new LoggingAfterAdvice());
+            factory.AddAdvice(new LoggingThrowsAdvice());
+            IUserOperateDal service = (IUserOperateDal)factory.GetProxy();
+            object result = service.UserLogin(username);*/
             //throw new Exception("我是BLL层异常内容啊");
             var userOperatedal = (UserOperateDal)GetAppContext.ApplicationContext.GetObject("UserOperateDal");
             return userOperatedal.UserLogin(username);
